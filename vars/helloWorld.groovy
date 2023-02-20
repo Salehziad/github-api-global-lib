35524602e48d78699ea42d0b426e4fdfb910e940
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-02-20 14:35:01 +0300

    add

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,8 @@
-def call(Map config = [:]) {
-    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+// def call(Map config = [:]) {
+//     //sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+//     sh 'echo hello world'
+// }
+
+def call() {
+    sh 'echo hello world'
 }
