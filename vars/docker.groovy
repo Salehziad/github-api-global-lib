cf5b1b8e4ad7517efc4744f66b77ef233c7f90e2
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-02-27 17:47:36 +0300

    add

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -46,10 +46,15 @@ def call(Map config = [:]) {
             }
             steps {
                 echo("I am in build")
-                script {
-                    myVar =execCommand:"git rev-list --tags --max-count=1"
-                    echo "MYVAR: ${myVar}"
-                }
+                // script {
+                //     myVar =execCommand:"git rev-list --tags --max-count=1"
+                //     echo "MYVAR: ${myVar}"
+                // }
+                GIT_COMMIT_EMAIL = sh (
+                script: 'git rev-list --tags --max-count=1',
+                returnStdout: true
+                ).trim()
+                echo "Git committer email: ${GIT_COMMIT_EMAIL}"
                 // sshPublisher(
                 //     continueOnError: false, failOnError: true,
                 //     publishers: [
