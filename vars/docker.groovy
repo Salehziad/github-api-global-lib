7d7c2c35528c17d0b80f52b54df9df7ed3fca8b9
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-02-27 12:26:37 +0300

    add

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,4 +1,34 @@
-
+pipeline {
+  agent any
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
+  environment {
+    DOCKERHUB_CREDENTIALS = credentials('dockerhub')
+  }
+  stages {
+    stage('Build') {
+      steps {
+        sh 'docker build . -t saleh99/ciam --no-cache'
+      }
+    }
+    stage('Login') {
+      steps {
+        sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+      }
+    }
+    stage('Push') {
+      steps {
+        sh 'docker push lloydmatereke/jenkins-docker-hub'
+      }
+    }
+  }
+  post {
+    always {
+      sh 'docker logout'
+    }
+  }
+}
 
 def call(Map config = [:]) {
     pipeline {
@@ -14,21 +44,9 @@ def call(Map config = [:]) {
             when {
                 branch 'release_4'
             }
-        stage('Build') {
-      steps {
+                 steps {
         sh 'docker build . -t saleh99/ciam --no-cache'
       }
-    }
-    stage('Login') {
-      steps {
-        sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-      }
-    }
-    stage('Push') {
-      steps {
-        sh 'docker push lloydmatereke/jenkins-docker-hub'
-      }
-    }
         }
     }
      post {
