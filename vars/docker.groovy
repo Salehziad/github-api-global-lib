d91fc111b2e94b4d3f0225940f9e851341ef50c9
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-02-27 12:27:15 +0300

    add

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,34 +1,34 @@
-pipeline {
-  agent any
-  options {
-    buildDiscarder(logRotator(numToKeepStr: '5'))
-  }
-  environment {
-    DOCKERHUB_CREDENTIALS = credentials('dockerhub')
-  }
-  stages {
-    stage('Build') {
-      steps {
-        sh 'docker build . -t saleh99/ciam --no-cache'
-      }
-    }
-    stage('Login') {
-      steps {
-        sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-      }
-    }
-    stage('Push') {
-      steps {
-        sh 'docker push lloydmatereke/jenkins-docker-hub'
-      }
-    }
-  }
-  post {
-    always {
-      sh 'docker logout'
-    }
-  }
-}
+// pipeline {
+//   agent any
+//   options {
+//     buildDiscarder(logRotator(numToKeepStr: '5'))
+//   }
+//   environment {
+//     DOCKERHUB_CREDENTIALS = credentials('dockerhub')
+//   }
+//   stages {
+//     stage('Build') {
+//       steps {
+//         sh 'docker build . -t saleh99/ciam --no-cache'
+//       }
+//     }
+//     stage('Login') {
+//       steps {
+//         sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+//       }
+//     }
+//     stage('Push') {
+//       steps {
+//         sh 'docker push lloydmatereke/jenkins-docker-hub'
+//       }
+//     }
+//   }
+//   post {
+//     always {
+//       sh 'docker logout'
+//     }
+//   }
+// }
 
 def call(Map config = [:]) {
     pipeline {
