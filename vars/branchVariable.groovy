8b7e0435ef7deab42e6870368bec978a35b7612f
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-02-28 11:53:47 +0300

    add

--- vars/branchVariable.groovy
+++ vars/branchVariable.groovy
@@ -2,26 +2,27 @@ def call(Map config = [: ]) {
   pipeline {
     agent any
     environment {
-      if (BRANCH_NAME == 'docker') {
-        ENVIRONMENT = 'docker'
-      } else {
-        ENVIRONMENT = 'dev'
-      }
+        if (BRANCH_NAME == 'origin/production') {
+            ENVIRONMENT = 'prod'
+        } else {
+            ENVIRONMENT = 'dev'
+        }
     }
     stages {
-      stage('Build') {
-        steps {
-          echo "Building branch ${BRANCH_NAME} for environment ${ENVIRONMENT}"
-          // Add build steps here
+        stage('Build') {
+            steps {
+                echo "Building branch ${BRANCH_NAME} for environment ${ENVIRONMENT}"
+                // Add build steps here
+            }
         }
-      }
-      stage('Deploy') {
-        steps {
-          echo "Deploying branch ${BRANCH_NAME} to environment ${ENVIRONMENT}"
-          // Add deployment steps here
+        stage('Deploy') {
+            steps {
+                echo "Deploying branch ${BRANCH_NAME} to environment ${ENVIRONMENT}"
+                // Add deployment steps here
+            }
         }
-      }
     }
-  }
+}
+
 
 }
