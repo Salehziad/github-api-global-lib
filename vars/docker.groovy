7fc434083b201b9dad8bc213a2e512eb7bfbad1b
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-02-28 15:27:29 +0300

    add

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,88 +1,61 @@
-// pipeline {
-//   agent any
-//   options {
-//     buildDiscarder(logRotator(numToKeepStr: '5'))
-//   }
-//   environment {
-//     DOCKERHUB_CREDENTIALS = credentials('dockerhub')
-//   }
-//   stages {
-//     stage('Build') {
-//       steps {
-//         sh 'docker build . -t saleh99/ciam --no-cache'
-//       }
-//     }
-//     stage('Login') {
-//       steps {
-//         sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
-//       }
-//     }
-//     stage('Push') {
-//       steps {
-//         sh 'docker push lloydmatereke/jenkins-docker-hub'
-//       }
-//     }
-//   }
-//   post {
-//     always {
-//       sh 'docker logout'
-//     }
-//   }
-// }
+def call(Map config = [: ]) {
+		pipeline {
+			agent any
+			environment {
+				BRANCH_NAME = "${GIT_BRANCH.replace('origin/','')}"
+			}
 
-def call(Map config = [:]) {
-    pipeline {
-    agent any
-      options {
-        ansiColor('xterm')
-    }
-    environment {
-    DOCKERHUB_CREDENTIALS = credentials('dockerHub')
-    }
-    stages {
-        stage('Deliver for development') {
-            when {
-                branch 'release_4'
-            }
-            // steps {
-            //     script {
-            //         def git_tag = sh(returnStdout: true, script: "git describe --abbrev=0 --tags").trim()
-            //         echo "Latest tag on the current branch: ${git_tag}"
-            //     }
-            // }
-            steps {
-                echo("I am in build")
-                sshPublisher(
-                    continueOnError: false, failOnError: true,
-                    publishers: [
-                    sshPublisherDesc(
-                        configName: "dev server",
-                        verbose: true,
-                        transfers: [
-                        // sshTransfer(
-                        //     execCommand:"docker image rm saleh99/ciam:latest"
-                        // ),
-                        sshTransfer(
-                            sourceFiles: "**/*",
-                            remoteDirectory: "ciam",
-                            execCommand:"cd /var/www/ciam && docker build . -t saleh99/ciam --no-cache"
-                        ),
-                        sshTransfer(
-                            execCommand: "docker push saleh99/ciam"
-                        ),
-                        // sshTransfer(
-                        //     execCommand: "echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin && docker push saleh99/ciam"
-                        // ),
-                        // sshTransfer(
-                        //     sourceFiles: "**/*",
-                        //     remoteDirectory: "${config.name}",
-                        //     execCommand:"cd /var/www/${config.name} && sudo npm i"
-                           
-                        // ),
-                    ])
-                ])
-            }
-        }
-    }
-}
+			stages {
+				stage('Set Environment Variables') {
+					steps {
+						script {
+							if (env.BRANCH_NAME == 'develop') {
+								env.ENVIRONMENT = 'dev server'
+							} else if (env.BRANCH_NAME == 'test') {
+								env.ENVIRONMENT = 'test server'
+							}
+						}
+					}
+				}
+
+				stage("Deliver for deploy") {
+					steps {
+						echo("I am in build")
+						sshPublisher(
+							continueOnError: false, failOnError: true,
+							publishers: [
+								sshPublisherDesc(
+									configName: ENVIRONMENT,
+									verbose: true,
+									transfers: [
+										sshTransfer(
+											sourceFiles: "**/*",
+											remoteDirectory: "ciam",
+											execCommand: "cd /var/www/ciam && docker build . -t saleh99/ciam --no-cache"
+										),
+										sshTransfer(
+											execCommand: "docker push saleh99/ciam"
+										),
+									])
+							])
+						//   echo("I am in Deploy")
+						//   sshPublisher(
+						//     continueOnError: false, failOnError: true,
+						//     publishers: [
+						//       sshPublisherDesc(
+						//         configName: ENVIRONMENT,
+						//         verbose: true,
+						//         transfers: [
+						//           sshTransfer(
+						//             execCommand: "cd /var/www/${config.name} && pm2 start"
+						//           )
+
+						//         ])
+						//     ])
+						// }
+					}
+				}
+			}
+
+		}
 }
