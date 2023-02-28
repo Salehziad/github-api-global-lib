2bdc8013273669a955de66cf1db9b14b07720545
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-02-28 09:32:34 +0300

    add

--- vars/tags.groovy
+++ vars/tags.groovy
@@ -1,12 +1,17 @@
 def call() {
-    pipeline {
+pipeline {
     agent any
     stages {
+        stage('Checkout repository') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: 'release_4']], userRemoteConfigs: [[url: 'https://github.com/agents-on-cloud/api.ciam.agentsoncloud.com.git']]])
+            }
+        }
         stage('Get latest tag') {
             steps {
                 script {
                     def git_tag = sh(returnStdout: true, script: "git describe --abbrev=0 --tags").trim()
-                    echo "Latest tag on the current branch: ${git_tag}"
+                    echo "Latest tag on the main branch: ${git_tag}"
                 }
             }
         }
