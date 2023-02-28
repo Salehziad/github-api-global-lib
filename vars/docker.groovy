30f5172c47352c4bd5169e8833619e81c9050ff2
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-02-28 15:31:49 +0300

    add

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,3 +1,55 @@
+def call(Map config = [:]) {
+    pipeline {
+    agent any
+      options {
+        ansiColor('xterm')
+    }
+    environment {
+    DOCKERHUB_CREDENTIALS = credentials('dockerHub')
+    }
+    stages {
+        stage('Deliver for development') {
+            when {
+                branch 'release_4'
+            }
+            steps {
+                echo("I am in build")
+                sshPublisher(
+                    continueOnError: false, failOnError: true,
+                    publishers: [
+                    sshPublisherDesc(
+                        configName: "dev server",
+                        verbose: true,
+                        transfers: [
+                        // sshTransfer(
+                        //     execCommand:"docker image rm saleh99/ciam:latest"
+                        // ),
+                        sshTransfer(
+                            sourceFiles: "**/*",
+                            remoteDirectory: "ciam",
+                            execCommand:"cd /var/www/ciam && docker build . -t saleh99/ciam --no-cache"
+                        ),
+                        sshTransfer(
+                            execCommand: "docker push saleh99/ciam"
+                        ),
+                        // sshTransfer(
+                        //     execCommand: "echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin && docker push saleh99/ciam"
+                        // ),
+                        // sshTransfer(
+                        //     sourceFiles: "**/*",
+                        //     remoteDirectory: "${config.name}",
+                        //     execCommand:"cd /var/www/${config.name} && sudo npm i"
+                           
+                        // ),
+                    ])
+                ])
+            }
+        }
+    }
+}
+}
+
+
 def call(Map config = [: ]) {
   pipeline {
     agent any
@@ -8,7 +60,7 @@ def call(Map config = [: ]) {
       stage('Set Environment Variables') {
         steps {
           script {
-            if (env.BRANCH_NAME == 'develop') {
+            if (env.BRANCH_NAME == 'docker') {
               env.ENVIRONMENT = 'dev server'
             } else if (env.BRANCH_NAME == 'test') {
               env.ENVIRONMENT = 'test server'
@@ -18,25 +70,7 @@ def call(Map config = [: ]) {
       }
       stage("Deliver for deploy")  {
         steps {
-          echo("I am in build")
-          sshPublisher(
-            continueOnError: false, failOnError: true,
-            publishers: [
-              sshPublisherDesc(
-                configName: ENVIRONMENT,
-                verbose: true,
-                transfers: [
-                        sshTransfer(
-                            sourceFiles: "**/*",
-                            remoteDirectory: "ciam",
-                            execCommand:"cd /var/www/ciam && docker build . -t saleh99/ciam --no-cache"
-                        ),
-                        sshTransfer(
-                            execCommand: "docker push saleh99/ciam"
-                        ),
-                ])
-            ])
-        //   echo("I am in Deploy")
+          echo("I am in build ${ENVIRONMENT}")
         //   sshPublisher(
         //     continueOnError: false, failOnError: true,
         //     publishers: [
@@ -45,12 +79,16 @@ def call(Map config = [: ]) {
         //         verbose: true,
         //         transfers: [
         //           sshTransfer(
-        //             execCommand: "cd /var/www/${config.name} && pm2 start"
-        //           )
+        //             execCommand: " rm -rf /var/www/${config.name}"
+        //           ),
+        //           sshTransfer(
+        //             sourceFiles: "**/*",
+        //             remoteDirectory: "${config.name}",
+        //             execCommand: "cd /var/www/${config.name} && sudo npm i"
 
+        //           ),
         //         ])
         //     ])
-        // }
       }
     }
   }
