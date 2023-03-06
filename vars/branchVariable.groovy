a6690e28ea9908d401ce4142587a47a0a9e6700c
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-03-06 08:39:05 +0300

    ADD

--- vars/branchVariable.groovy
+++ vars/branchVariable.groovy
@@ -10,6 +10,7 @@ def call(Map config = [: ]) {
           script {
             if (env.BRANCH_NAME == 'develop') {
               env.ENVIRONMENT = 'dev server'
+              env.CHANNEL='#development-jenkins'
             } else if (env.BRANCH_NAME == 'test') {
               env.ENVIRONMENT = 'test server'
             }
@@ -53,6 +54,23 @@ def call(Map config = [: ]) {
             ])
         }
       }
+    }
+    post {
+        always {
+           slackSend channel: CHANNEL, message: "${env.JOB_NAME}  - #${env.BUILD_NUMBER} : Started . ", color: "good"
+        }
+        success {
+          slackSend channel: CHANNEL, message: "${env.JOB_NAME}  - #${env.BUILD_NUMBER} : Build Finshed Success . ", color: "good"
+        }
+        unstable {
+           slackSend channel: CHANNEL, message: "${env.JOB_NAME}  - #${env.BUILD_NUMBER} : Build have Error  . ", color: "warning"
+        }
+        failure {
+            slackSend channel: CHANNEL, message: "${env.JOB_NAME}  - #${env.BUILD_NUMBER} : Failure Build  . ", color: "danger"
+        }
+        changed {
+             echo 'Things were different before...'
+        }
     }
   }
 
